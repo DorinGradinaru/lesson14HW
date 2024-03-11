e531ef6ffdabb3789492cbad7bc19d9187eca0e3
DorinGradinaru <dorin.gradinaru@icloud.com>
2024-03-11 11:47:34 +0200

    first

---
+++ index.js
@@ -0,0 +1,38 @@
+// function SumAndRadical(a, b, c) {
+
+//   const sum = a + b + c 
+//   if (sum >= 0) {
+//     const radical = Math.sqrt(sum)
+
+//     return radical
+//   } else {
+//     return "Error"
+//   }
+// }
+// console.log(SumAndRadical(1, 2, 3)) 
+// console.log(SumAndRadical(5, 10, 15))  
+// console.log(SumAndRadical(-1, -2, -3))
+
+
+
+
+
+// function RandomLetter(str) {
+  
+//   const randomIndex = Math.floor(Math.random() * str.length)
+//   return str[randomIndex]
+// }
+// const str = 'ceva text'
+// const randomLetter = RandomLetter(str)
+// console.log(randomLetter)
+
+
+
+
+
+
+const str1 = 'hello'
+const str2 = 'world'
+let concatinate = str1.concat(str2)
+console.log(concatinate)
+
